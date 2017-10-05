a3838f4b97f064e69e20c27ca6acba8e70010ab1
yusuf-farah <yfarah1577@gmail.com>
2017-10-05 13:17:19 -0400

    rework the question of favorite color

--- week2_day3_methods/Program.cs
+++ week2_day3_methods/Program.cs
@@ -10,6 +10,43 @@ namespace week2_day3_methods
     {
         static void Main(string[] args)
         {
+
+            //Console.WriteLine("please enter a name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("please enter the best food ever!");
+            //string food = Console.ReadLine();
+            //FavoriteFood(name, food);
+            //FavoriteFood(" yusuf", "pasta");
+            //FavoriteFood("ali", "rice");
+            //FavoriteFood("ahmed", "kfc");
+            RetirementCalculater(40);
+
         }
+        public static void RetirementCalculater(int userAge)
+        {
+            int retirementAge = Math.Max(0, 65 - userAge);
+            Console.WriteLine("the use will retire in " + retirementAge+ "userAge");
+        }
+
+        public static void FavoriteFood(string name , string food)
+        {
+           Console.WriteLine(name + "favorite food is" + food);
+            Console.WriteLine("**********************************");
+            
+        } //end favorite
+
+        public static int add(int firstName, int secondNumber)
+        {
+            int sum = firstName + secondNumber;
+            return sum;
+    
+            
+            
+            
+            
+                
+
+    }
     }
 }
+
